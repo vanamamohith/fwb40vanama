5f6bb46a75a90c03f76dac124174a89ac82dd868
vanamamohith <saimohith.97@gmail.com>
2021-11-03 17:57:14 -0500

    changes in app.js

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var usaRouter = require('./routes/usa');
 var addmodsRouter = require('./routes/addmods');
 var selectorRouter = require('./routes/selector');
 
@@ -25,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/usa', usaRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
 

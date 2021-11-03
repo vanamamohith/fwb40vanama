cea79868752add90b892d19b74ecbf9af4a1ac95
vanamamohith <saimohith.97@gmail.com>
2021-11-03 18:02:29 -0500

    changes in app.js

--- routes/USA.js
+++ routes/USA.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("usa", { title: "Search Results USA" });
+  res.render("usa", { title: "hello USA" });
 });
 
 module.exports = router;

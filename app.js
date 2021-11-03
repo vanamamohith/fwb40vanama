560cd532fc65d56d8139291e6cab2c0bd9454fe1
vanamamohith <saimohith.97@gmail.com>
2021-11-03 17:59:18 -0500

    changes in app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var addmodsRouter = require('./routes/addmods');
 var selectorRouter = require('./routes/selector');
+var usaRouter = require('./routes/usa');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
+app.use('/usa', usaRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

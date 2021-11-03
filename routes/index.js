49988d8635bf2de7fdafb995239313c1f53b610f
vanamamohith <saimohith.97@gmail.com>
2021-11-02 21:47:46 -0500

    added

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var client_count = 0;
-
-function update()
-{
-  client_count++;
-  document.getElementById('target').innerHTML='Value Now '+ client_count;
-}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
